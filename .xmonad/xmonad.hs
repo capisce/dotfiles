9b67267a61e54725f4f2e4656ece741813751fb6
Samuel Rødal <srodal@gmail.com>
2017-06-04 13:56:13 +0000

    Xmonad customizations with xmobar

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,11 +1,25 @@
 import XMonad
+import XMonad.Config.Desktop
+import XMonad.Hooks.DynamicLog
 import XMonad.Util.EZConfig
 
-main = xmonad $ defaultConfig
-  { modMask = mod4Mask
-  , terminal = "konsole"
-  }
-  `additionalKeysP`
-  [ ("M-e", spawn "emacs")
-  , ("M-b", spawn "google-chrome-stable")
-  ]
+main = do
+  spawn "xmobar"
+
+  xmonad $ desktopConfig
+      { borderWidth = 2
+      , focusFollowsMouse = False
+      , focusedBorderColor = colorFocusedBorder
+      , normalBorderColor = colorNormalBorder
+      , modMask = mod4Mask
+      , terminal = "konsole"
+      }
+      `additionalKeysP`
+        [ ("M-e", spawn "emacs")
+        , ("M-c", spawn "google-chrome-stable")
+        , ("M-p", spawn "dmenu_run -fn 'monospace-18' -b")
+        , ("M-q", spawn "killall xmobar; xmonad --recompile && xmonad --restart")
+        ]
+
+colorNormalBorder = "#CCCCC6"
+colorFocusedBorder = "#FD971F"
--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
@@ -10,7 +6,6 @@
       /etc/nixos/hardware-configuration.nix
     ];
 
-  # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
@@ -28,21 +23,14 @@
   boot.loader.grub.device = "/dev/sda";
 
   networking.hostName = "nixus";
-  # networking.wireless.enable = true;
   networking.networkmanager.enable = true;
 
-  # Select internationalisation properties.
   i18n = {
     consoleFont = "Lat2-Terminus16";
     consoleKeyMap = "en-latin9";
     defaultLocale = "en_US.UTF-8";
   };
 
-  # Set your time zone.
-  # time.timeZone = "Europe/Amsterdam";
-
-  # List packages installed in system profile. To search by name, run:
-  # $ nix-env -qaP | grep wget
   environment.systemPackages = with pkgs; [
     coreutils
     curl
@@ -51,7 +39,9 @@
     haskellPackages.xmonad
     haskellPackages.xmonad-extras
     haskellPackages.xmonad-contrib
+    htop
     kdeApplications.konsole
+    psmisc
     vim
     wget
     xclip
@@ -73,6 +63,7 @@
     synaptics = {
       enable = true;
       twoFingerScroll = true;
+      tapButtons = false;
 
       accelFactor = "0.015";
       minSpeed = "0.8";
@@ -100,11 +91,6 @@
       slim.defaultUser = "srodal";
     };
 
-    # displayManager.sessionCommands = with pkgs; lib.mkAfter
-    #   ''
-    #   xmodmap /path/to/.Xmodmap
-    #   '';
-
     windowManager.default = "xmonad";
 
     desktopManager.default = "none";
@@ -113,7 +99,6 @@
     xkbVariant = "colemak";
   };
 
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.extraUsers.srodal = {
     name = "srodal";
     extraGroups = [
